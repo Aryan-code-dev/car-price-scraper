745c19a8978c6ea26e7bebb596abc99e80245fc5
Aryan Dande <86364775+Aryan-code-dev@users.noreply.github.com>
2022-04-24 12:41:53 +0530

    updated_app.py

--- app.py
+++ app.py
@@ -1,6 +1,57 @@
 from flask import request
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
 from flask import Flask, render_template
+def scrape(name_fil, year_fil, price_fil):
+    driver = webdriver.Firefox(executable_path=r"C:\Users\Aryan Dande\Downloads\gecko\geckodriver.exe")
+    #url = "https://www.cardekho.com/used-cars+in+pune"
+    #url = "https://www.cars24.com/buy-used-car?sort=P&storeCityId=2423&pinId=411001"
+    #url=" https://droom.in/cars/used"
+    #url="https://www.olx.in/pune_g4059014/cars_c84"
+    #url = " https://www.ola.cars/listings/buy-used+city-is-pune"
+    #url="https://www.spinny.com/used-cars-in-pune/s/"
+    #url="https://www.carwale.com/used/cars-in-pune/"
+    urls = ["https://www.cardekho.com/used-cars+in+pune"]
+
+    for j in range(1):
+        driver.maximize_window()
+        driver.get(urls[j])
+        time.sleep(2)  # Allow 2 seconds for the web page to open
+        driver.find_element(By.CSS_SELECTOR, "[title^='Petrol']").click()
+        driver.find_element(By.CSS_SELECTOR, "[title^='Manual']").click()
+        scroll_pause_time = 1  # You can set your own pause time. My laptop is a bit slow so I use 1 sec
+        screen_height = driver.execute_script("return window.screen.height;")  # get the screen height of the web
+        i = 1
+
+        while True:
+            # scroll one screen height each time
+            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+            i += 1
+            time.sleep(scroll_pause_time)
+            # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")
+            # Break the loop when the height we need to scroll to is larger than the total scroll height
+            if screen_height * i > scroll_height:
+                break
+
+        content = driver.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(content,"html.parser")
+        time.sleep(5)
+        if j == 0:
+            set3 = soup.find_all("div", class_="holder hover",limit=100)
+            for k in set3:
+                print(k.getText())
+        elif j == 1:
+            set3 = soup.find_all("div", class_="_1l4fi")
+            for k in set3:
+                print(k.getText())
+        else:
+            set3 = soup.find_all("div", class_="styles__carDetailContainer")
+            for k in set3:
+                print(k.getText())
+
 
 app = Flask(__name__)
 
@@ -9,6 +60,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/',methods=['POST'])
 def getvalue():
     name = request.form['first']
@@ -16,10 +68,11 @@ def getvalue():
     price = request.form['third']    
 
     print(name+" "+year+" "+price)
-
+    scrape(name, year, price)
     return render_template('index.html')
-
 
 
 if __name__ == "__main__":
     app.run()
+
+
